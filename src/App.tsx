a14f08b975f24fe3ebedf3195a02555501a88f5d
emmanuel-abreu <eabreue1@gmail.com>
2022-08-29 23:17:02 -0400

    reinit to allow for typescript starter files; created navbar component

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Portfolio site is currently in progress.</h1>
+      <p>Currently working through The Odin Project.  Some other things i'm 
+        aiming to work on: </p>
+      <ul>
+        <li>
+          Finance/budgeting application that can connect with other external
+          APIs
+        </li>
+        <li>Dashboard/bulletin board with custom plugins</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
