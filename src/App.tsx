13c4fe3a27c5a9029147284afae563dd139f1cd5
emmanuel-abreu <eabreue1@gmail.com>
2022-09-14 14:54:26 -0400

    fixed some syntax issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
 
   return (
     <div className="App">
+      <Navbar/>
       <h1>Portfolio site is currently in progress.</h1>
     </div>
   );
